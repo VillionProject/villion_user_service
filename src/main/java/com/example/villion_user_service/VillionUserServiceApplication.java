28e4b80ba8c961caa5843b2e0ad49cc77e9622d0
sesam-me <lsi6601@gmail.com>
2024-04-18 23:40:44 +0900

    first commit

---
+++ src/main/java/com/example/villion_user_service/VillionUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.villion_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillionUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillionUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/villion_user_service/VillionUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villion_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillionUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
