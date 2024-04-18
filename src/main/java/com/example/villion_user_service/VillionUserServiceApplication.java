6d2cce2c4fc6039844caf8787f2f97d49ff34b87
sesam-me <lsi6601@gmail.com>
2024-04-19 02:00:25 +0900

    #1 eruka 설정

--- src/main/java/com/example/villion_user_service/VillionUserServiceApplication.java
+++ src/main/java/com/example/villion_user_service/VillionUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.villion_user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VillionUserServiceApplication {
 
 	public static void main(String[] args) {
